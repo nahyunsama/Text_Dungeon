43e3e9beb9872901eee3ba34707edff72b3b996b
hamnahyun <hamnahyun@nyahyun.com>
2025-12-30 00:34:13 +0900

    Refactoring User Movie

--- app/game.py
+++ app/game.py
@@ -14,6 +14,59 @@ class Game:
         print("")
         move_string = str(input())
         print("")
+        match move_string:
+            case "1":
+                self.user_xy_before = self.user_xy
+                self.user_xy[1] = self.user_xy[1] - 1
+                if self.map[self.user_xy[0]][self.user_xy[1]] != "=":
+                    self.map[self.user_xy[0]][self.user_xy[1]] = "U"
+                    self.map[self.user_xy_before[0]][self.user_xy_before[1]] = " "
+                    self.user_xy_before = [0, 0]
+                else:
+                    print("> Here is the wall!")
+                    print("")
+                    self.user_xy = self.user_xy_before.copy()
+                    self.user_xy_before = [0, 0]
+            
+            case "2":
+                self.user_xy_before = self.user_xy.copy()
+                self.user_xy[0] = self.user_xy[0] - 1
+                if self.map[self.user_xy[0]][self.user_xy[1]] != "=":
+                    self.map[self.user_xy[0]][self.user_xy[1]] = "U"
+                    self.map[self.user_xy_before[0]][self.user_xy_before[1]] = " "
+                    self.user_xy_before = [0, 0]
+                else:
+                    print("> Here is the wall!")
+                    print("")
+                    self.user_xy = self.user_xy_before.copy()
+                    self.user_xy_before = [0, 0]
+
+            case "3":
+                self.user_xy_before = self.user_xy.copy()
+                self.user_xy[0] = self.user_xy[0] + 1
+                if self.map[self.user_xy[0]][self.user_xy[1]] != "=":
+                    self.map[self.user_xy[0]][self.user_xy[1]] = "U"
+                    self.map[self.user_xy_before[0]][self.user_xy_before[1]] = " "
+                    self.user_xy_before = [0, 0]
+                else:
+                    print("> Here is the wall!")
+                    print("")
+                    self.user_xy = self.user_xy_before.copy()
+                    self.user_xy_before = [0, 0]
+
+            case "4":
+                self.user_xy_before = self.user_xy.copy()
+                self.user_xy[1] = self.user_xy[1] + 1
+                if self.map[self.user_xy[0]][self.user_xy[1]] != "=":
+                    self.map[self.user_xy[0]][self.user_xy[1]] = "U"
+                    self.map[self.user_xy_before[0]][self.user_xy_before[1]] = " "
+                    self.user_xy_before = [0, 0]
+                else:
+                    print("> Here is the wall!")
+                    print("")
+                    self.user_xy = self.user_xy_before.copy()
+                    self.user_xy_before = [0, 0]
+        return self.user_xy, self.user_xy_before
 
     def run(self):
         while True:
@@ -30,6 +83,8 @@ class Game:
                 self.map = Map.STAGE_1
                 self.lore = Map.STAGE_1_LORE
                 print(self.lore)
+                (self.user_x, self.user_y, self.user_x_before, self.user_y_before,
+                 self.monster_x, self.monster_y)= Map.unit_xy(self.stage)
                 Map.map_print(self.map)
                 self.input_move()
 
--- app/map.py
+++ app/map.py
@@ -1,11 +1,27 @@
 class Map:
 
     STAGE_1 = [["=","=","=","=","="],
-               ["="," ","*"," ","="],
-               ["="," ","U"," ","="],
-               ["=","=","=","=","="]]
+                   ["="," ","*"," ","="],
+                   ["="," ","U"," ","="],
+                    ["=","=","=","=","="]]
 
     STAGE_1_LORE = "Let's Tutrial"
+
+    @staticmethod
+    def unit_xy(stage):
+        match stage:
+            case 1:
+                user_x = 2
+                user_y = 2
+                user_x_before = 0
+                user_y_before = 0
+                monster_x = 1
+                monster_y = 2
+                #user_xy_before = [0,0]
+                #user_xy = [2,2]
+                #monster_xy = [1,2]
+        
+        return user_x, user_y, user_x_before, user_y_before, monster_x, monster_y
 
     @staticmethod
     def map_print(map_list):
