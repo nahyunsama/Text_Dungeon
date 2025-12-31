cc37db83f98c9ac146139ee8a236480b148c5fa5
hamnahyun <hamnahyun@nyahyun.com>
2025-12-31 11:50:05 +0900

    Refactoring the Left move

--- app/game.py
+++ app/game.py
@@ -1,10 +1,11 @@
 from .map import Map
+import copy
 
 class Game:
 
     def __init__(self):
         self.stage = 0
-
+        self.init_flag = 0
     def input_move(self):
         print("> choice your move")
         print("> left  : 1")
@@ -16,17 +17,21 @@ class Game:
         print("")
         match move_string:
             case "1":
-                self.user_xy_before = self.user_xy
-                self.user_xy[1] = self.user_xy[1] - 1
-                if self.map[self.user_xy[0]][self.user_xy[1]] != "=":
-                    self.map[self.user_xy[0]][self.user_xy[1]] = "U"
-                    self.map[self.user_xy_before[0]][self.user_xy_before[1]] = " "
-                    self.user_xy_before = [0, 0]
+                self.user_x_before = self.user_x
+                self.user_y_before = self.user_y
+                self.user_y = self.user_y - 1
+                if self.map[self.user_x][self.user_y] != "=":
+                    self.map[self.user_x][self.user_y] = "U"
+                    self.map[self.user_x_before][self.user_y_before] = " "
+                    self.user_x_before = 0
+                    self.user_y_before = 0
                 else:
                     print("> Here is the wall!")
                     print("")
-                    self.user_xy = self.user_xy_before.copy()
-                    self.user_xy_before = [0, 0]
+                    self.user_x = self.user_x_before
+                    self.user_y = self.user_y_before
+                    self.user_x_before = 0
+                    self.user_y_before = 0
             
             case "2":
                 self.user_xy_before = self.user_xy.copy()
@@ -66,7 +71,6 @@ class Game:
                     print("")
                     self.user_xy = self.user_xy_before.copy()
                     self.user_xy_before = [0, 0]
-        return self.user_xy, self.user_xy_before
 
     def run(self):
         while True:
@@ -75,17 +79,20 @@ class Game:
                 print("> PRESS ANY KEY")
                 print("")
                 input()
-            
                 print("")
                 print("> Game Start!")
                 self.stage = 1
+                self.init_flag = 1
             elif self.stage == 1:
-                self.map = Map.STAGE_1
-                self.lore = Map.STAGE_1_LORE
-                print(self.lore)
-                (self.user_x, self.user_y, self.user_x_before, self.user_y_before,
-                 self.monster_x, self.monster_y)= Map.unit_xy(self.stage)
-                Map.map_print(self.map)
-                self.input_move()
+                if self.init_flag == 1:
+                    self.map = copy.deepcopy(Map.STAGE_1)
+                    self.lore = Map.STAGE_1_LORE
+                    print(self.lore)
+                    (self.user_x, self.user_y, self.user_x_before, self.user_y_before,
+                    self.monster_x, self.monster_y) = Map.unit_xy(self.stage)
+                    self.init_flag = 0
+                else:
+                    Map.map_print(self.map)
+                    self.input_move()
 
 
